763e9bccc3cbcc7382b152e24ff623eedf3f3b79
whiteSkar <sypower07@hotmail.com>
2015-11-21 11:12:26 -0800

    Fix vscode to load .net 3.5

--- cheesy_mouse/Assets/VSCode/Plugins/Editor/VSCode.cs
+++ cheesy_mouse/Assets/VSCode/Plugins/Editor/VSCode.cs
@@ -714,11 +714,13 @@ namespace dotBunny.Unity
             if (content.Length == 0)
                 return "";
 
+            /*
             // Make sure our reference framework is 2.0, still the base for Unity
             if (content.IndexOf("<TargetFrameworkVersion>v3.5</TargetFrameworkVersion>") != -1)
             {
                 content = Regex.Replace(content, "<TargetFrameworkVersion>v3.5</TargetFrameworkVersion>", "<TargetFrameworkVersion>v2.0</TargetFrameworkVersion>");
             }
+            */
 
             string targetPath = "";// "<TargetPath>Temp" + Path.DirectorySeparatorChar + "bin" + Path.DirectorySeparatorChar + "Debug" + Path.DirectorySeparatorChar + "</TargetPath>"; //OutputPath
             string langVersion = "<LangVersion>default</LangVersion>";
