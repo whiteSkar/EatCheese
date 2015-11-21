d4b217a2fd5210fcaaca690c90182aae650eb058
whiteSkar <sypower07@hotmail.com>
2015-11-20 21:56:01 -0800

    map with only outerwall physics, player, player animation, and movement

---
+++ cheesy_mouse/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+
+    void Move(Vector2 dir)
+    {
+        Vector2 curP = transform.position;
+        Vector2 newP = Vector2.MoveTowards(curP, curP + dir, moveSpeed * Time.deltaTime);
+        
+        GetComponent<Rigidbody2D>().MovePosition(newP);
+        
+        Animator animator = GetComponent<Animator>();
+        animator.SetFloat("DirX", dir.x);
+        animator.SetFloat("DirY", dir.y);
+    }
+    
+    bool CanMove(Vector2 dir)
+    {
+        Vector2 pos = transform.position;
+        RaycastHit2D hit = Physics2D.Linecast(pos + dir, pos);
+        return hit.collider == GetComponent<Collider2D>();
+    }
+
+    void Start()
+    {
+
+    }
+
+
+    void FixedUpdate()
+    {
+        int horizontal = (int) Input.GetAxis("Horizontal");
+        int vertical = (int) Input.GetAxis("Vertical");
+        
+        if (horizontal != 0)
+            vertical = 0;
+            
+        Vector2 dir = new Vector2(horizontal, vertical);
+        
+        if (CanMove(dir))
+            Move(dir);
+    }
+}
