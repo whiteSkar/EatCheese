0880cdbd2453b700677d67170c8808b0ce005100
whiteSkar <sypower07@hotmail.com>
2015-11-20 22:05:38 -0800

    added cheese

---
+++ cheesy_mouse/Assets/CheeseController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheeseController : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+            gameObject.SetActive(false);
+    }
+}
